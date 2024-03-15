05df18d7cbdce9932ece1167fab4a6f4e7d42585
João Victor Andrade Rosendo da Paz <joao.paz@produs.com.br>
2024-03-15 10:12:03 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'dicionario_biografico',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
